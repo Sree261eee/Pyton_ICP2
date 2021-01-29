4c53197d0aca0ae35885568ebad5808cd4a6cf55
srikanth <sy99b@mail.umkc.edu>
2021-01-29 17:09:32 -0600

    .

--- prob_3.py
+++ prob_3.py
@@ -3,9 +3,9 @@ d = dict()
 
 for line in infile:
     line = line.strip()
-    print(line)
+    # print(line)
     words = line.split(" ")
-    print(words)
+    # print(words)
     for word in words:
         if word in d:
             d[word] = d[word]+1
@@ -16,7 +16,7 @@ print ("\n The total numbers in file are : ", d)
 
 for key in list(d.keys()):
     str1 = key+":"+ str(d[key])
-    print(str1)
+    # print(str1)
     text1 = open("word.txt", "a")
     text1.write('\n')
     text1.write(str1)
